10c7ca7722b815ee4274a45421297f4288a3cd7e
GAVENESH A/L BATUMALAI <ggavenesh@gmail.com>
2026-09-01 00:50:12 +0800

    Commit "Web icon"

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [tailwindcss(), tanstackStart(), viteReact()],
 });
